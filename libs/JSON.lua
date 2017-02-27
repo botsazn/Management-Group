2b50fe38444e4a07eed84977964923509978d969
botsazn <info.botsazn@gmail.com>
2017-02-27 07:24:04 -0800

    pequerobot.com

--- libs/JSON.lua
+++ libs/JSON.lua
@@ -1052,8 +1052,5 @@ return OBJDEF:new()
 --   20100731.1    initial public release
 --
 
--- کد های پایین در ربات نشان داده نمیشوند
--- http://permag.ir
--- @permag_ir
--- @permag_bots
--- @permag
+-- @PequeRobotCH
+-- http://pequerobot.com
